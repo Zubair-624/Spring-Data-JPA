86339ca7f1952eeabcefeb27910a84fd2dbc956e
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-02-16 12:03:39 +0600

    feat(user): Add new field and annotation for better data handling

--- src/main/java/com/example/springdatajpa01/model/User.java
+++ src/main/java/com/example/springdatajpa01/model/User.java
@@ -1,24 +1,40 @@
 package com.example.springdatajpa01.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
-    private String name;
+    @NotBlank(message = "Name is required")
+    @Column(nullable = false)
+    private String userName;
 
-    private String email;
+    @Email(message = "Email should be valid")
+    @Column(unique = true, nullable = false)
+    private String userEmail;
 
-    private String phoneNumber;
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+?[0-9]\\d{1,14}", message = "Phone number must be exactly 15 digits")
+    @Column(unique = true, nullable = false)
+    private String userPhoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id_fk", referencedColumnName = "id", nullable = false)
+    private Address address;
 }
