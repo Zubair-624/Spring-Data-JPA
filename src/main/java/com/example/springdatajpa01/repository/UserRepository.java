ff027c19bc914682b6023e54757a6e69891da27d
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-02-17 23:11:53 +0600

    feat: Add UserRepository for database access

---
+++ src/main/java/com/example/springdatajpa01/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.springdatajpa01.repository;
+
+import com.example.springdatajpa01.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
