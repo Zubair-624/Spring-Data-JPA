d469dadb29b1f9a49e07cc2a3203049fa0805d70
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-02-18 23:44:32 +0600

    feat: Add @Builder to Address, update UserDto, fix email method

--- src/main/java/com/example/springdatajpa01/dto/UserDto.java
+++ src/main/java/com/example/springdatajpa01/dto/UserDto.java
@@ -1,30 +1,32 @@
 package com.example.springdatajpa01.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDto {
 
-    private Long id;
+    private Long userId;
 
     @NotBlank(message = "Name is required")
-    private String name;
+    private String userName;
 
-    @NotBlank(message = "Email should be valid")
-    private String email;
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String userEmail;
 
     @NotBlank(message = "Phone number is required")
-    @Pattern(regexp = "^\\+?[0-9]\\d{1,14}", message = "Phone number must be between 10-15 digits")
-    private String phoneNumber;
+    @Pattern(regexp = "^\\+?[0-9]\\d{1,14}$", message = "Phone number must be between 10-15 digits")
+    private String userPhoneNumber;
 
     private String street;
     private String city;
     private String state;
     private String country;
+    private String zipCode;
 }
--- src/main/java/com/example/springdatajpa01/model/Address.java
+++ src/main/java/com/example/springdatajpa01/model/Address.java
@@ -2,12 +2,14 @@ package com.example.springdatajpa01.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 public class Address {
 
@@ -16,7 +18,7 @@ public class Address {
     private Long id;
 
     @Column(nullable = false)
-    private String Street;
+    private String street;
 
     @Column(nullable = false)
     private String state;
