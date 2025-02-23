b92006e414e4e1331aea3c1dd7b634ad838a3e39
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-02-23 22:16:26 +0600

    test(UserService): Add unit tests for createUser with validation and exceptions

---
+++ src/test/java/com/example/springdatajpa01/UserServiceTest.java
@@ -0,0 +1,93 @@
+package com.example.springdatajpa01;
+
+import com.example.springdatajpa01.dto.UserDto;
+import com.example.springdatajpa01.model.Address;
+import com.example.springdatajpa01.model.User;
+import com.example.springdatajpa01.repository.UserRepository;
+import com.example.springdatajpa01.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserService userService;
+    private User user;
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp(){
+
+        Address address = Address.builder()
+                .street("123 Main st")
+                .state("NY")
+                .city("New York")
+                .country("USA")
+                .zipCode("10001")
+                .build();
+
+        user = User.builder()
+                .userId(1L)
+                .userName("Zubair Mazumder")
+                .userEmail("zubair@gmail.com")
+                .userPhoneNumber("+08801903123131")
+                .address(address)
+                .build();
+
+        userDto = UserDto.builder()
+                .userId(1L)
+                .userName("Zubair Mazumder")
+                .userEmail("zubair@gmail.com")
+                .userPhoneNumber("+08801903123131")
+                .street("123 Main st")
+                .state("NY")
+                .city("New York")
+                .country("USA")
+                .zipCode("10001")
+                .build();
+    }
+
+    /** -------------------- Test: Create User -------------------- **/
+
+    @Test
+    void shouldCreateUserSuccessfully(){
+
+        when(userRepository.existsByUserEmail(userDto.getUserEmail())).thenReturn(false);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        UserDto createdUser = userService.createUser(userDto);
+
+        assertNotNull(createdUser);
+        assertEquals(userDto.getUserEmail(), createdUser.getUserEmail());
+        verify(userRepository, times(1)).save(any(User.class));
+
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUserEmailIsAlreadyExist() {
+
+        when(userRepository.existsByUserEmail(userDto.getUserEmail())).thenReturn(true);
+
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+            userService.createUser(userDto);
+        });
+
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+        assertEquals("Email already exists", exception.getReason());
+
+
+    }
+}
